c5a11f9e5ec20edce872a1219f8dedd25e1e2d4a
Pleasure Ndhlovu <2017269214@ufs4life.ac.za>
2021-06-15 10:52:02 +0200

    AppDbContext for application & Repository pattern added

---
+++ Data/CRepositoryPattern.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PsDevs.Data
+{
+    public class CRepositoryPattern<T> : IRepositoryPattern<T> where T : class
+    {
+        public AppDbContext _appDbContext { get; set; }
+        public CRepositoryPattern(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void Create(T entity)
+        {
+            _appDbContext.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _appDbContext.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _appDbContext.Set<T>().Update(entity);
+        }
+
+        public void Save()
+        {
+            _appDbContext.SaveChanges();
+        }
+
+        public IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return _appDbContext.Set<T>().Where(expression);
+        }
+
+        public IEnumerable<T> FindAll()
+        {
+            return _appDbContext.Set<T>();
+        }
+
+        public T FindById(int id)
+        {
+            return _appDbContext.Set<T>().Find(id);
+        }
+    }
+}
---
+++ Data/IRepositoryPattern.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace PsDevs.Data
+{
+    public interface IRepositoryPattern<T>
+    {
+        public void Create(T entity);
+        public void Delete(T entity);
+        public void Update(T entity);
+        public void Save();
+        public IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        public IEnumerable<T> FindAll();
+        public T FindById(int id);
+    }
+}
